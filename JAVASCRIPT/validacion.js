4fdd5ac079616a972afb2aecf11a996d925ba1fd
Iris Ivana Figueroa <figuesco15@gmail.com>
2024-05-15 22:44:37 -0600

    validación de form

---
+++ JAVASCRIPT/validacion.js
@@ -0,0 +1,8 @@
+const $imgArticles = document.querySelector("#imgArticles");
+$category = document.querySelector("#category"),
+$desArticle = document.querySelector("#desArticle");
+
+$imgArticles.onsubmit = evento => {
+    console.log("Subido");
+};
+
