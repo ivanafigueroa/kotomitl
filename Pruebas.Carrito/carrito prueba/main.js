fd7ebf3b05bfd24424f420d94eea55114b38d233
victoriamorenoq <morenoquintanilla.060589@gmail.com>
2024-05-17 20:19:32 -0600

    pruebas de tarea7.carrito de compras

---
+++ Pruebas.Carrito/carrito prueba/main.js
@@ -0,0 +1,105 @@
+const CARDS_CONTAINER = document.querySelector("#card-container");
+
+PRODUCTOS =[ {
+    "id": 1,
+    "producto": "Blusa",
+    "precio": 100,
+    "imagen": "blusa1.jpg"
+},
+    {
+        "id": 2,
+        "producto": "Blusa ",
+        "precio": 200,
+        "imagen":"blusa2.jpg"
+    },
+    {
+        "id": 3,
+        "producto": "Chamarra",
+        "precio": 300,
+        "imagen": "chamarra1.jpg"
+    },
+    {
+        "id": 4,
+        "producto": " Jeans",
+        "precio": 400,
+        "imagen": "jeans1.jpeg"
+    },
+    {
+        "id": 5,
+        "producto": "Jeans ",
+        "precio": 500,
+        "imagen": "jeans2.jpeg"
+    },
+    {
+        "id": 6,
+        "producto": "Pijama",
+        "precio": 600,
+        "imagen":"pijama1.jpeg"
+    },
+    {
+        "id": 7,
+        "producto": "Pijama",
+        "precio": 700,
+        "imagen": "pijama2.jpeg"
+    },
+    {
+        "id": 8,
+        "producto": "Short",
+        "precio": 800,
+        "imagen": "short1.jpeg"
+    },
+    {
+        "id": 9,
+        "producto": "Short ",
+        "precio": 900,
+        "imagen": "short2.jpg"
+
+    },
+    {
+        "id": 10,
+        "producto": "Sudadera",
+        "precio": 1000,
+        "imagen": "sudadera1.jpg"
+    }
+]
+
+function createElements() {
+    const cardObj = {
+      img_section: document.createElement("img"),
+      name_section: document.createElement("h3"),
+      precio_section: document.createElement("p"),
+      id_section: document.createElement("p"),
+      
+    };
+    return cardObj;
+
+  }
+
+
+  function injectData(obj, producto) {
+    obj.img_section.src = `./Assets/${producto.imagen}`;
+    obj.name_section.textContent = producto.producto;
+    obj.precio_section.textContent = `Precio: $ ${producto.precio}`;
+    obj.id_section.textContent = `id: ${producto.id}`;
+    
+
+    renderCard(obj);
+  }
+  
+  function renderCard(cardObj) {
+    const card = document.createElement("div");
+    card.append(
+      cardObj.img_section,
+      cardObj.name_section,
+      cardObj.precio_section,
+      cardObj.id_section,
+    
+      );
+    card.className = "div-creado";
+    CARDS_CONTAINER.appendChild(card);
+  }
+  
+  PRODUCTOS.forEach((producto) => {
+    const cardData = createElements();
+    injectData(cardData, producto);
+  });
