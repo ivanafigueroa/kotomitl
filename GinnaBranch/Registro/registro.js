ecdc55ba18d52e6e7a1e4222d4ab5f15f896e901
Ginna Pg <itzandypg@gmail.com>
2024-05-28 12:59:42 -0600

    Cambios y pagina de pago

--- GinnaBranch/Registro/registro.js
+++ GinnaBranch/Registro/registro.js
@@ -1,18 +1,29 @@
-function validarFormulario() {
-    var nombre = document.getElementById("nombre").value;
-    var telefono = document.getElementById("telefono").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    if (nombre === '' || telefono === '' || email === '' || password === '') {
-        alert("Por favor, complete todos los campos.");
-        return false;
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevenir la acción predeterminada del formulario
+    
+    // Obtener los valores del formulario
+    var nombre = document.getElementById("nombre-usuario").value.trim();
+    var apellidos = document.getElementById("apellidos").value.trim();
+    var telefono = document.getElementById("numero-telefono").value.trim();
+    var email = document.getElementById("emailInput").value.trim();
+    var password = document.getElementById("passwordInput").value.trim();
+    var tycCheck = document.getElementById("tycCheck").checked;
+  
+    // Validación
+    if (nombre === "" || apellidos === "" || telefono === "" || email === "" || password === "" || !tycCheck) {
+      alert("Por favor, complete todos los campos y acepte los términos y condiciones.");
+    } else if (!validateEmail(email)) {
+      alert("Por favor, introduzca una dirección de correo electrónico válida.");
+    } else {
+      alert("¡Datos correctos! El registro se realizará ahora.");
+      // Aquí podrías agregar código para enviar el formulario a través de AJAX o realizar otras acciones
+      // En este ejemplo, simplemente mostramos la alerta de que los datos son correctos
     }
-
-    var telefonoRegex = /^\d{10}$/;
-    if (!telefonoRegex.test(telefono)) {
-        alert("Por favor, ingrese un número de teléfono válido de 10 dígitos.");
-        return false;
-    }
-
-    return true; 
+  });
+  
+  // Función para validar el formato de correo electrónico
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+  
